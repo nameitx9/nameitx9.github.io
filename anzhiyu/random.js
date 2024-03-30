693248f5f642fed325e40e2d575ec63b56436ab1
nameitx <therootboy@outlook.com>
2024-03-30 12:01:08 +0800

    Site updated: 2024-03-30 12:01:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/29/hello-world/"];function toRandomPost(){
+var posts=["2024/03/30/使用ssh代理连接到github持久化问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
